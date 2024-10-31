5609878a47fd2f1afc5a5e25f1b741340294ec73
gbrousset <85172595+gbrousset@users.noreply.github.com>
2024-10-31 14:14:47 +0100

    Update TicketList.js

--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -11,7 +11,7 @@ const TicketList = () => {
     useEffect(() => {
         const fetchTickets = async () => {
             try {
-                const response = await axios.get('http://localhost:3001/api/tickets', { withCredentials: true });
+                const response = await axios.get('https://prjfinal-studi-frontend.onrender.com/api/tickets', { withCredentials: true });
                 setTickets(response.data);
             } catch (err) {
                 console.error('Erreur lors de la récupération des billets:', err);
