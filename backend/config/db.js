fb9ec0e1e98b60dee5df24b365332227a024ebe5
gbrousset <85172595+gbrousset@users.noreply.github.com>
2024-10-31 13:10:42 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,13 @@ const { Pool } = require('pg');
 
 // Créer une instance du pool de connexions
 const pool = new Pool({
-    user: 'postgres', // A remplacer par le nom d'utilisateur de la base de données
-    host: 'localhost',     // L'hôte de la base de données
-    database: 'billeterie_jo_db', // A Remplacer par le nom de la base de données
-    password: 'azertlove', // A Remplacer par le mot de passe de la base de données
-    port: 5432,           // Le port par défaut de PostgreSQL
+    user: 'billeterie_jo_db_user', // Nom d'utilisateur de la base de données
+    host: 'dpg-cshk9jo8fa8c739f2r0g-a.frankfurt-postgres.render.com', // L'hôte de la base de données
+    database: 'billeterie_jo_db', // Nom de la base de données
+    password: 'e4Nqx4gTGEbPsAojRT5HDMkjTn7eimkR', // Mot de passe de la base de données
+    port: 5432, // Le port par défaut de PostgreSQL
 });
 
 // Exporter le pool pour l'utiliser dans d'autres fichiers
 module.exports = pool;
+
